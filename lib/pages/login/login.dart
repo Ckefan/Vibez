672a70548b3023a64d50d0ad4ceafdba6855cb4d
Ckefan <290591923@qq.com>
2020-11-07 23:32:52 +0800

    登录页开发

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -5,16 +5,61 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget avatar = Image.asset(
-      "lib/assets/images/vibez-pattern.png",
-      width: double.infinity,
-      height: 71.85,
+    final avatar = "lib/assets/images/vibez-pattern.png";
+    final fontColor = TextStyle(
+      color: Color.fromRGBO(184, 184, 184, 1),
     );
-    return Container(
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage(avatar),
+            fit: BoxFit.fill,
+          ),
+        ),
         width: double.infinity,
         height: double.infinity,
-        margin: EdgeInsets.only(top: 78.16),
-        color: Colors.black,
-        child: avatar);
+        child: Column(
+          children: <Widget>[
+            Text(
+              'LOG IN',
+              style: TextStyle(fontSize: 40.0, color: Colors.white),
+            ),
+            TextField(
+              autofocus: true,
+              style:fontColor,
+              decoration: InputDecoration(
+                hintText: 'USERNAME',
+                hintStyle: fontColor
+              ),
+            ),
+            TextField(
+              autofocus: false,
+              style: fontColor,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'PASSWORD',
+                hintStyle: fontColor
+              ),
+            ),
+            Text(
+              'Forgot Login Info?',
+              style: TextStyle(fontSize: 40.0),
+            ),
+            RaisedButton(
+              child: Text("LOG IN"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text('Cancel'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
