e207615673374ffc4978970896a33e2f48832c77
陈棚 <290591923@qq.com>
2020-11-19 11:09:10 +0800

    解决win10环境下java报错的问题

--- android/app/src/main/kotlin/com/example/Vibez/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Vibez
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Vibez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
