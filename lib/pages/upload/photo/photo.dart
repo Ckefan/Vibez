58929cc252cacb226df6bf4ec6440ea6f564edb7
陈棚 <290591923@qq.com>
2021-01-03 19:09:10 +0800

    添加ios 访问相机和图册权限

--- lib/pages/upload/photo/photo.dart
+++ lib/pages/upload/photo/photo.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path/path.dart';
+import 'package:image/image.dart' as imageLib;
+import 'package:image_picker/image_picker.dart';
+import 'package:photofilters/photofilters.dart';
 
 class Photo extends StatefulWidget {
   @override
@@ -9,23 +15,61 @@ class Photo extends StatefulWidget {
 
 class _PhotoState extends State<Photo> {
   CameraController controller;
+  String fileName;
+  File imageFile;
+  List<Filter> filters = presetFiltersList;
+
   var cameras;
 
-  @override
-  void initState(){
-    super.initState();
-    initCamera();
-  }
-
   Future initCamera() async {
-    cameras = await availableCameras();
-     controller = CameraController(cameras[0], ResolutionPreset.medium);
-      controller.initialize().then((_) {
+    cameras = await availableCameras(); //获取相机列表
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
       if (!mounted) {
         return;
       }
       setState(() {});
     });
+  }
+
+  //打开本地相册，选取图片
+  Future getGalleryImage(context) async {
+    final pickedFile =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+    print(pickedFile);
+
+    imageFile = File(pickedFile.path);
+    print(imageFile);
+
+    fileName = basename(imageFile.path);
+    var image = imageLib.decodeImage(imageFile.readAsBytesSync());
+    image = imageLib.copyResize(image, width: 600);
+    Map imagefile = await Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (context) => new PhotoFilterSelector(
+          title: Text(""),
+          image: image,
+          filters: presetFiltersList,
+          filename: fileName,
+          loader: Center(child: CircularProgressIndicator()),
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+    if (imagefile != null && imagefile.containsKey('image_filtered')) {
+      setState(() {
+        imageFile = imagefile['image_filtered'];
+      });
+      print(imageFile.path);
+    }
+  }
+
+  @override
+  void initState() {
+    print(presetFiltersList);
+    super.initState();
+    initCamera();
   }
 
   @override
@@ -36,9 +80,156 @@ class _PhotoState extends State<Photo> {
 
   @override
   Widget build(BuildContext context) {
-    return controller?.value?.isInitialized? AspectRatio(
-        aspectRatio:
-        controller.value.aspectRatio,
-        child: CameraPreview(controller)):Container();
+    final _isInitialized = controller?.value?.isInitialized;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: _isInitialized != null
+                  ? AspectRatio(
+                      aspectRatio: MediaQuery.of(context).size.width /
+                          MediaQuery.of(context).size.height,
+                      child: CameraPreview(controller))
+                  : imageFile != null
+                      ? Image.file(
+                          imageFile,
+                          width: 300,
+                          height: 500,
+                        )
+                      : SizedBox(),
+            ),
+            Positioned(
+              top: 30,
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.keyboard_arrow_left,
+                        size: 30, color: Colors.white),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Text(
+                    "Photo",
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              top: 65,
+              right: 20,
+              height: 150.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    child: Image.asset(
+                      'lib/assets/images/Switch Camera Icon.png',
+                      width: 17,
+                      height: 20,
+                    ),
+                    onTap: () {},
+                  ),
+                  GestureDetector(
+                    child: Image.asset(
+                      'lib/assets/images/Flash Off Icon.png',
+                      width: 17,
+                      height: 20,
+                    ),
+                    onTap: () {},
+                  ),
+                  GestureDetector(
+                    child: Image.asset(
+                      'lib/assets/images/Timer Icon.png',
+                      width: 17,
+                      height: 20,
+                    ),
+                    onTap: () {},
+                  ),
+                  GestureDetector(
+                    child: Image.asset(
+                      'lib/assets/images/Filters Icon.png',
+                      width: 24,
+                      height: 20,
+                    ),
+                    onTap: () {},
+                  )
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 60,
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(width: 27.0, height: 32.0),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        width: 95.0,
+                        height: 95.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 4.0,
+                            color: Color.fromRGBO(41, 169, 224, 1),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 80.0,
+                        height: 80.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        width: 50.0,
+                        height: 55.0,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(0, 0, 0, 0.2),
+                                offset: Offset(0, 4.0),
+                                blurRadius: 10.0)
+                          ],
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'lib/assets/images/Photo Upload Icon.png',
+                            ),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () => getGalleryImage(context),
+                    child: Image.asset('lib/assets/images/Upload Post Icon.png',
+                        width: 27.0, height: 32.0),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
